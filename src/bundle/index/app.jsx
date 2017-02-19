d8d2d72bf9d2d9587435d0eff21a182b81d8ef0a
alichen <ali322@gmail.com>
2017-02-20 00:04:41 +0800

    change entry

--- src/bundle/index/app.jsx
+++ src/bundle/index/app.jsx
@@ -3,7 +3,6 @@ import rootReducer from './reducer.es6'
 
 export default () => {
     const store = configureStore(rootReducer, { eventReducer: { events: [], repo: '' } })
-
     if (module.hot) {
         module.hot.accept('./reducer.es6', () => {
             const nextReducer = require('./reducer.es6')
--- src/bundle/index/index.jsx
+++ src/bundle/index/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { match, hashHistory, Router } from 'react-router'
+import { match, browserHistory, Router } from 'react-router'
 import app from './app.jsx'
 import routes from './routes.jsx'
 
 const App = app()(Router)
-
-match({ history: hashHistory, routes }, (err, redirectLocation, renderProps) => {
-    render(<App {...renderProps} history={hashHistory} />, document.getElementById('app'))
+if (module.hot) {
+  module.hot.accept()
+}
+match({ history: browserHistory, routes }, (err, redirectLocation, renderProps) => {
+    render(<App {...renderProps} history={browserHistory} />, document.getElementById('app'))
 })
