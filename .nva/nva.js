73c5457567ba29cf20e9b9ff705990d2e95004c5
alichen <ali322@gmail.com>
2017-04-28 11:55:04 +0800

    fix pass

---
+++ .nva/nva.js
@@ -0,0 +1,25 @@
+const baseConf = {
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm.js'
+        }
+    }
+}
+
+export default {
+    type: "frontend",
+    spa: true,
+    entryJSExt: ".js",
+    entryCSSExt: ".styl",
+    beforeDev(config) {
+        return baseConf
+    },
+    beforeBuild(config) {
+        return baseConf
+    },
+    beforeVendor(config) {
+        return config.map(v => {
+            return v.name === 'js' ? baseConf : null
+        })
+    }
+}
