dc0482d98361ce34a724de0f22ff004bd6502faa
alichen <ali322@gmail.com>
2017-05-02 14:52:57 +0800

    fix for v0.2.10

--- test/e2e/server.js
+++ test/e2e/server.js
@@ -3,7 +3,8 @@ let http = require('http')
 
 let app = nvaServer({
     path: "dist",
-    asset: 'dist'
+    asset: 'dist',
+    rewrites: true
 })
 
 let server = http.createServer(app).listen(8080)
--- test/e2e/spec/demo.js
+++ test/e2e/spec/demo.js
@@ -2,7 +2,7 @@ module.exports = {
     'find answer': (browser) => {
         let link = '//*[@id="app"]/div/div/div[3]/div[1]/div/span/p[1]/a'
         let btn = '//*[@id="app"]/div/div/div[1]/button'
-        browser.url('http://127.0.0.1:8080/index').maximizeWindow()
+        browser.url('http://127.0.0.1:8080/').maximizeWindow()
         browser.expect.element('body').be.present
         browser.useXpath().waitForElementVisible(link,16000)
         browser.pause(5000)
