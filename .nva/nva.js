0fa81637c74782d3c66d82de232123fc50f5e48c
alichen <ali322@gmail.com>
2017-05-04 16:51:03 +0800

    fix for v0.2.19

--- .nva/nva.js
+++ .nva/nva.js
@@ -1,12 +1,5 @@
 module.exports = {
     type: "frontend",
     entryJSExt: ".jsx",
-    entryCSSExt: ".styl",
-    beforeDev(conf) {
-        let entry = {}
-        for (let k in conf.entry) {
-            entry[k] = ['react-hot-loader/patch'].concat(conf.entry[k])
-        }
-        return { entry }
-    }
+    entryCSSExt: ".styl"
 }
