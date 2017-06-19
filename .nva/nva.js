db71ae3fe77dbbe01c767d0fd598ca75ae530c36
alichen <ali322@gmail.com>
2017-06-19 15:29:03 +0800

    fix pass

--- .nva/nva.js
+++ .nva/nva.js
@@ -1,27 +1,7 @@
-const baseConf = {
-    resolve: {
-        alias: {
-            'vue': 'vue/dist/vue.esm.js',
-            "vue-router": 'vue-router/dist/vue-router.esm.js'
-        }
-    }
-}
-
 module.exports = {
     type: "frontend",
     spa: true,
     jsExt: ".js",
     mock: require('./mock'),
-    cssExt: ".styl",
-    beforeDev(config) {
-        return baseConf
-    },
-    beforeBuild(config) {
-        return baseConf
-    },
-    beforeVendor(config) {
-        return config.map(v => {
-            return v.name === 'js' ? baseConf : null
-        })
-    }
+    cssExt: ".styl"
 }
