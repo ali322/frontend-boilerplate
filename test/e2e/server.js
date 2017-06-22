759746574635ac2adb93ce68ce8bf063b06dd773
alichen <ali322@gmail.com>
2017-06-22 16:50:36 +0800

    fix pass

--- test/e2e/server.js
+++ test/e2e/server.js
@@ -5,7 +5,7 @@ let app = nvaServer({
     path: 'dist',
     asset: 'dist',
     rewrites: true,
-    mock: require('../../.nva/mock/event')
+    mock: '.nva/mock/*.json'
 })
 
 let server = http.createServer(app).listen(8080)
