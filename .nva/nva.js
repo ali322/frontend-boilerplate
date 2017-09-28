513edd9125a435b55c0c21c1c79f971457f9283f
alichen <ali322@gmail.com>
2017-09-28 17:01:48 +0800

    fix pass

--- .nva/nva.js
+++ .nva/nva.js
@@ -1,6 +1,6 @@
 module.exports = {
-    type: "frontend",
-    jsExt: ".js",
-    autocheck: ['vue', 'vuex'],
-    cssExt: ".styl"
+  type: 'frontend',
+  jsExt: '.js',
+  autocheck: ['vue', 'vuex'],
+  cssExt: '.styl'
 }
