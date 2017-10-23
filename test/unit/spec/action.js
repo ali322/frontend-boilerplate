8048b9553ee56172c91c1dadb69a2b9da16ac5b1
alichen <ali322@gmail.com>
2017-10-23 11:34:24 +0800

    fix pass

--- test/unit/spec/action.js
+++ test/unit/spec/action.js
@@ -1,32 +1,34 @@
-import test from "ava"
-import nock from "nock"
-import configureStore from "redux-mock-store"
-import thunkMiddleware from "redux-thunk"
-import * as actions from "../../../src/index/module/action"
-import * as constants from "../../../src/index/module/constant"
+import test from 'ava'
+import nock from 'nock'
+import configureStore from 'redux-mock-store'
+import thunkMiddleware from 'redux-thunk'
+import * as actions from '../../../src/index/module/action'
+import * as constants from '../../../src/index/module/constant'
 
 let mockStore
 
 test.before(() => {
-    nock.cleanAll()
-    mockStore = configureStore([thunkMiddleware]);
+  nock.cleanAll()
+  mockStore = configureStore([thunkMiddleware])
 })
 
-test("should RESPONSE_EVENTS when fetched", t => {
-    let ret = {
-        status: 'ok',
-        data: []
-    }
-    let initialState = {
-        eventReducer: { events: [] }
-    }
-    const store = mockStore(initialState)
-    let expectedActions = [
-        { type: constants.REQUEST_EVENTS },
-        { type: constants.RESPONSE_EVENTS, payload: ret.data }
-    ]
-    nock("http://localhost").get("/mock/events").reply(200, ret)
-    return store.dispatch(actions.fetchEvents()).then(() => {
-        t.deepEqual(store.getActions(), expectedActions)
-    })
+test('should RESPONSE_EVENTS when fetched', t => {
+  let ret = {
+    status: 'ok',
+    data: []
+  }
+  let initialState = {
+    eventReducer: { events: [] }
+  }
+  const store = mockStore(initialState)
+  let expectedActions = [
+    { type: constants.REQUEST_EVENTS },
+    { type: constants.RESPONSE_EVENTS, payload: ret.data }
+  ]
+  nock('http://localhost')
+    .get('/mock/events')
+    .reply(200, ret)
+  return store.dispatch(actions.fetchEvents()).then(() => {
+    t.deepEqual(store.getActions(), expectedActions)
+  })
 })
--- test/unit/spec/reducer.js
+++ test/unit/spec/reducer.js
@@ -1,13 +1,13 @@
-import test from "ava"
+import test from 'ava'
 
-import eventReducer from "../../../src/index/module/reducer"
-import * as constants from "../../../src/index/module/constant"
+import eventReducer from '../../../src/index/module/reducer'
+import * as constants from '../../../src/index/module/constant'
 
-test("should handle RESPONSE_EVENTS", t => {
-    let action = {
-        type: constants.RESPONSE_EVENTS,
-        payload: []
-    }
-    let nextState = eventReducer({ events: [] }, action)
-    t.is(nextState.eventsFetched, true)
+test('should handle RESPONSE_EVENTS', t => {
+  let action = {
+    type: constants.RESPONSE_EVENTS,
+    payload: []
+  }
+  let nextState = eventReducer({ events: [] }, action)
+  t.is(nextState.eventsFetched, true)
 })
