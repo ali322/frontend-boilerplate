e83a7979bcb16a575bdb2cce4681733442a090c9
alichen <ali322@gmail.com>
2017-12-21 16:36:50 +0800

    add pwa support

--- .nva/nva.js
+++ .nva/nva.js
@@ -1,7 +1,21 @@
+let OfflinePlugin = require("offline-plugin")
+let glob = require("glob")
+require('dist/vendor.json')
+
 module.exports = {
-  type: 'frontend',
-  spa: true,
-  autocheck: ['vue', 'vuex', 'vue-router', 'vuex-router-sync'],
-  jsExt: '.js',
-  cssExt: '.styl'
+    type: "frontend",
+    spa: true,
+    //   autocheck: ['vue', 'vuex', 'vue-router', 'vuex-router-sync'],
+    jsExt: ".js",
+    cssExt: ".styl",
+    beforeBuild(conf) {
+        let vendors = []
+        vendors = glob.sync("dist/vendor/*.@(js|css)")
+        vendors = vendors.map(v => v.replace("dist", ""))
+
+        let offlinePlugin = new OfflinePlugin({
+            externals: ["/"].concat(vendors)
+        })
+        return { plugins: conf.plugins.concat([offlinePlugin]) }
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import app from './container'
 import './common/responsive'
+import * as offlinePluginRuntime from 'offline-plugin/runtime'
+
+offlinePluginRuntime.install()
 
 if (module.hot) {
   module.hot.accept()
