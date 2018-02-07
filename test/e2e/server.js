c81495dc6f746d992a72653fd1b4409b41b034a7
alichen <ali322@gmail.com>
2018-02-07 11:13:39 +0800

    fix test-e2e

--- test/e2e/server.js
+++ test/e2e/server.js
@@ -1,12 +1,15 @@
 let nvaServer = require('nva-server')
 let http = require('http')
 
+process.chdir('dist')
+
 let app = nvaServer({
-    path: 'dist',
-    asset: 'dist',
-    mock: '.nva/mock/*.json'
+    path: '.',
+    asset: '.',
+    rewrites: true,
+    mock: { path: '../.nva/mock/*.js' }
 })
 
-let server = http.createServer(app).listen(8080)
+let server = http.createServer(app).listen(9090)
 
 module.exports = server
