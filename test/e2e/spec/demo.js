fe25ebcd2e3fb562e74f3a2ec9526e44b3a99f6a
alichen <ali322@gmail.com>
2018-02-15 16:33:57 +0800

    fix eslint in test

--- test/e2e/spec/demo.js
+++ test/e2e/spec/demo.js
@@ -1,7 +1,9 @@
 import { Selector } from 'testcafe'
 
+// eslint-disable-next-line no-undef
 fixture('get started').page('http://127.0.0.1:9090/')
 
+// eslint-disable-next-line no-undef
 test('my first test', async t => {
-   await t.expect(Selector('.event').count).gt(0)
+  await t.expect(Selector('.event').count).gt(0)
 })
