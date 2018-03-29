dce906fbc8417e80a73e0e371b7847510ff5d94a
alichen <ali322@gmail.com>
2018-03-29 14:14:15 +0800

    fix pass

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  process: runner => runner.startApp('node ./test/e2e/server.js'),
+  process: runner => runner.startApp('node ./test/e2e/server.js', 3000),
   spec: ['./test/e2e/spec/demo.js']
 }
