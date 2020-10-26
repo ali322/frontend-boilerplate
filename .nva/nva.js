47d8cf52e113864dd83ea30cf6c42a541949b1cf
alichen <ali322@gmail.com>
2020-10-26 16:34:32 +0800

    fix pass

--- .nva/nva.js
+++ .nva/nva.js
@@ -1,46 +1,37 @@
 // let OfflinePlugin = require("offline-plugin")
 // let glob = require("glob")
 const { name } = require('../package.json')
+const subappURL = 'http://localhost:5000/'
+
+function conf() {
+  return {
+      output: {
+          // publicPath: subappURL, // used by subapp
+          library: `${name}-[name]`,
+          libraryTarget: "umd"
+      }
+  };
+}
 
 module.exports = {
     type: "frontend",
     spa: true,
-    strict: true,
+    strict: false,
     autocheck: ['vue', 'vuex', 'vue-router', 'vuex-router-sync'],
     jsExt: ".js",
     cssExt: ".styl",
-    outputPrefix: '/',
     beforeDev() {
-      return {
-        output: {
-            // publicPath: subappURL, // used by subapp
-            library: `${name}-[name]`,
-            libraryTarget: 'umd',
-            // jsonpFunction: `webpackJsonp_${name}`
-        } 
-      }
+      return conf()
     },
     beforeBuild() {
-      return {
-        output: {
-            // publicPath: subappURL, // used by subapp
-            library: `${name}-[name]`,
-            libraryTarget: 'umd',
-            // jsonpFunction: `webpackJsonp_${name}`
-        } 
+      let result = conf()
+      result.output = {
+        publicPath: '/'
       }
-    }
-    //hmrPath: subappURL, // used by subapp
-    //imagePrefix: url => `${subappURL}/asset/image/${url}`, // used by subapp
-    //fontPrefix: url => `${subappURL}/asset/font/${url}`, // used by subapp
-    // beforeBuild(conf) {
-    //     let vendors = []
-    //     vendors = glob.sync("dist/vendor/*.@(js|css)")
-    //     vendors = vendors.map(v => v.replace("dist", ""))
-
-    //     let offlinePlugin = new OfflinePlugin({
-    //         externals: ["/"].concat(vendors)
-    //     })
-    //     return { plugins: conf.plugins.concat([offlinePlugin]) }
-    // }
+      return result
+    },
+    outputPrefix: '/',
+    // hmrPath: subappURL, // used by subapp
+    // imagePrefix: url => `${subappURL}/asset/image/${url}`, // used by subapp
+    // fontPrefix: url => `${subappURL}/asset/font/${url}`, // used by subapp
 }
